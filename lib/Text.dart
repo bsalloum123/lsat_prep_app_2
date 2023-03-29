291d0d44dba7bdec3932c4324808187c27fb905c
bsalloum123 <bsalloum@muhlenberg.edu>
2023-03-28 23:38:02 -0400

    new profile page

--- lib/Text.dart
+++ lib/Text.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Text extends StatelessWidget{
-  final String _title;
+  final String title;
   final String _body;
   static const double _hPad = 20.0;
 
-  const Text(this._title, this._body, {super.key}); // constructor
+  const Text(this.title, this._body, {super.key}); // constructor
   @override
   Widget build(BuildContext context){
     return Column(
@@ -14,7 +14,7 @@ class Text extends StatelessWidget{
       children:[
        Container(
          padding: const EdgeInsets.fromLTRB(_hPad, 32.0, _hPad, 4.0),
-         child: Text(_title),
+         child: Text(title),
              //style: Theme.of(context).textTheme.title),
        ),
        Container(
---
+++ lib/profile_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:lsat_prep_app_2/settings.dart';
+import 'homePage.dart';
+
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Profile',
+        theme: lsatTheme,
+        home:
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Profile'),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.help_outline),
+                tooltip: 'Show help',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('This is a help bar')));
+                },
+              ),
+            ],
+          ),
+          body:
+          ListView(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.green[300],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            minRadius: 60.0,
+                            child: CircleAvatar(
+                              radius: 50.0,
+                              backgroundImage:
+                              AssetImage(''),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Name',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Username',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        'Email',
+                        style: TextStyle(
+                          color: Colors.green[200],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        'email...',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text(
+                        'temp1',
+                        style: TextStyle(
+                          color: Colors.green[200],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '...',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text(
+                        'temp2',
+                        style: TextStyle(
+                          color: Colors.green[200],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '...',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: NavigationBar(
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: 'home'),
+              NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
+              NavigationDestination(icon: Icon(Icons.settings), label: 'settings'),
+            ],
+            onDestinationSelected: (int index){
+              if (index == 0){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) {
+                  return const homeDesign();
+                }));
+              }
+              if (index == 1){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) {
+                  return const ProfilePage();
+                }));
+              }
+              if (index == 2){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) {
+                  return const settings();
+                }));
+              }
+              // setState(() {
+              //   currentPage = index;
+              // });
+            },
+            // selectedIndex: currentPage,
+          ),
+        ),
+      );
+
+  }
+}
