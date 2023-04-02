6d41c32d4a0641918f7c4ed91b50e207f7d1ebd3
Christian Johansson <cjohansson@muhlenberg.edu>
2023-04-02 18:02:18 -0400

    Category Recognition quiz

--- lib/CategoryRecognition.dart
+++ lib/CategoryRecognition.dart
@@ -1,70 +1,182 @@
 import 'package:flutter/material.dart';
+import 'CatRec/answer.dart';
+import 'CatRec/question.dart';
+import 'CatRec/quiz.dart';
+import 'CatRec/result.dart';
 
-class cateRecogDesign extends StatelessWidget {
+class cateRecogDesign extends StatefulWidget {
   const cateRecogDesign({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const cateRecogPage();
+  State<StatefulWidget> createState() {
+    return _cateRecogState();
   }
 }
 
-class cateRecogPage extends StatefulWidget {
+/*class cateRecogPage extends StatefulWidget {
   const cateRecogPage({Key? key}) : super(key: key);
 
   @override
   State<cateRecogPage> createState() => _cateRecogState();
-}
+}*/
 
-class _cateRecogState extends State<cateRecogPage> {
-  //int currentPage = 0;
+class _cateRecogState extends State<cateRecogDesign> {
+  final _questions = const [
+    {
+      'questionText': 'Q1. Who created Flutter?',
+      'answers': [
+        {'text': 'Facebook', 'score': -2},
+        {'text': 'Adobe', 'score': -2},
+        {'text': 'Google', 'score': 10},
+        {'text': 'Microsoft', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q2. What is Flutter?',
+      'answers': [
+        {'text': 'Android Development Kit', 'score': -2},
+        {'text': 'IOS Development Kit', 'score': -2},
+        {'text': 'Web Development Kit', 'score': -2},
+        {
+          'text':
+          'SDK to build beautiful IOS, Android, Web & Desktop Native Apps',
+          'score': 10
+        },
+      ],
+    },
+    {
+      'questionText': ' Q3. Which programing language is used by Flutter',
+      'answers': [
+        {'text': 'Ruby', 'score': -2},
+        {'text': 'Dart', 'score': 10},
+        {'text': 'C++', 'score': -2},
+        {'text': 'Kotlin', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q4. Who created Dart programing language?',
+      'answers': [
+        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
+        {'text': 'Brendan Eich', 'score': -2},
+        {'text': 'Bjarne Stroustrup', 'score': -2},
+        {'text': 'Jeremy Ashkenas', 'score': -2},
+      ],
+    },
+    {
+      'questionText':
+      'Q5. Is Flutter for Web and Desktop available in stable version?',
+      'answers': [
+        {
+          'text': 'Yes',
+          'score': -2,
+        },
+        {'text': 'No', 'score': 10},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    // ignore: avoid_print
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      // ignore: avoid_print
+      print('We have more questions!');
+    } else {
+      // ignore: avoid_print
+      print('No more questions!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Category Recognition'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.help_outline),
-            tooltip: 'Show help',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a help bar')));
-            },
+        appBar: AppBar(
+          title: const Text('Category Recognition'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.help_outline),
+              tooltip: 'Show help',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a help bar')));
+              },
+            ),
+          ],
+        ),
+        body:
+        Center(
+          child: Container(
+            margin: const EdgeInsets.all(15.0), // adding margin
+            child:
+            Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child:
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: _questionIndex < _questions.length ? Quiz(
+                          answerQuestion: _answerQuestion,
+                          questionIndex: _questionIndex,
+                          questions: _questions,
+                        ) //Quiz
+                            : Result(_totalScore, _resetQuiz),
+                      ),
+                    ),
+                  ),
+                ]
+            ),
           ),
-        ],
-      ),
-      body: const cateRecogBody()
+        ),
     );
   }
 }
 
-class cateRecogBody extends StatelessWidget {
+/*class cateRecogBody extends StatelessWidget {
   const cateRecogBody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: const EdgeInsets.all(15.0), // adding margin
         child:
         Column(
-            children:[
+            children: [
               Expanded(
                 child: SingleChildScrollView(
                   scrollDirection: Axis.vertical,
-                  child: Text(
-                    "Category Recognition stuff",
-                    style: TextStyle(
-                      color: Colors.red.shade900,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20.0,
-                    ),
+                  child:
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: _questionIndex < _questions.length ? Quiz(
+                      answerQuestion: _answerQuestion,
+                      questionIndex: _questionIndex,
+                      questions: _questions,
+                    ) //Quiz
+                        : Result(_totalScore, _resetQuiz),
                   ),
                 ),
-              ),]
+              ),
+            ]
         ),
       ),
     );
   }
-}
+}*/
