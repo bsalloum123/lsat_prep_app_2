2bc8982d0b46ca419d1b33543b65a90259e82160
Inaya Alkhatib <inayahill@gmail.com>
2023-04-09 18:13:22 -0400

    Updated list on category page

--- lib/CategoryRecognition.dart
+++ lib/CategoryRecognition.dart
@@ -1,12 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 //import 'CatRec/answer.dart';
 //import 'CatRec/question.dart';
 import 'CatRec/quiz.dart';
 import 'CatRec/result.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:lsat_prep_app_2/main.dart';
-
+makeList() {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  var newList = [];
+  newList.length = 0;
+  db.collection('catRegLR').orderBy("id_string").limit(3).get().then(
+        (querySnapshot) {
+      print("Successfully completed");
+      for (var docSnapshot in querySnapshot.docs) {
+        newList.add({
+          "contextText": docSnapshot.data()["context"],
+          "question": docSnapshot.data()["question"],
+          "answers": [
+            {'text': 'Identify a Flaw', 'score': -1},
+            {'text': 'Strengthen', 'score': -1},
+            {'text': 'Implication', 'score': 1},
+            {'text': 'Sufficient Assumptions', 'score': -1},
+          ]
+        });
+      }
+      return newList;
+    },
+    onError: (e) => print("Error completing: $e"),
+  );
+}
+final temp = makeList();
 
 class cateRecogDesign extends StatefulWidget {
   const cateRecogDesign({Key? key}) : super(key: key);
@@ -18,34 +41,15 @@ class cateRecogDesign extends StatefulWidget {
   }
 }
 
-class _cateRecogState extends State<cateRecogDesign> {
-  FirebaseFirestore db = FirebaseFirestore.instance;
+// class makeListClass {
+//
+//
+//   // get newList => makeList();
+// }
 
-  void makeMap() {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    Map<String, dynamic> newMap= {};
-    db.collection('catRegLR').orderBy("id_string").limit(3).get().then(
-          (querySnapshot) {
-        print("Successfully completed");
-        for (var docSnapshot in querySnapshot.docs) {
-          // print(docSnapshot.data());
-          newMap.addAll({
-            "contextText": docSnapshot.data()["context"],
-            "question": docSnapshot.data()["question"],
-            "answers": [
-              {'text': 'Identify a Flaw', 'score': -1},
-              {'text': 'Strengthen', 'score': -1},
-              {'text': 'Implication', 'score': 1},
-              {'text': 'Sufficient Assumptions', 'score': -1},
-            ],
-          });
-        }
-        print(newMap);
-        return newMap;
-      },
-      onError: (e) => print("Error completing: $e"),
-    );
-  }
+class _cateRecogState extends State<cateRecogDesign> {
+  // FirebaseFirestore db = FirebaseFirestore.instance;
+  // final temp = makeList();
 
   // get userExist => db.collection('users');
   //
@@ -61,68 +65,67 @@ class _cateRecogState extends State<cateRecogDesign> {
   //   );
   // }
   // // get _questions => questionMap(db);
-  get _questions=> makeMap();
 
-  // final _questions = const [
-  //   {
-  //     'contextText':
-  //         'Negotiations between the union and the trucking industry will not resume unless one or the other of these parties agrees to make concessions. The trucking industry will not make any concessions without certain prior concessions being made by the union, and the union will make concessions only if the government agrees to act as mediator.',
-  //     'questionText': '1. If the statements above are true, which one of the following must also be true on the basis of them?',
-  //     'answers': [
-  //       {'text': 'Identify a Flaw', 'score': -2},
-  //       {'text': 'Strengthen', 'score': -2},
-  //       {'text': 'Implication', 'score': 10},
-  //       {'text': 'Sufficient Assumptions', 'score': -2},
-  //     ],
-  //   },
-  //   {
-  //     'contextText':
-  //         'Scientists analyzing air bubbles that had been trapped in Antarctic ice during the Earth\'s last ice age found that the ice-age atmosphere had contained unusually large amounts of ferrous material and surprisingly small amounts of carbon dioxide. One scientist noted that algae absorb carbon dioxide from the atmosphere. The scientist hypothesized that the ferrous material, which was contained in atmospheric dust, had promoted a great increase in the population of Antarctic algae such as diatoms.',
-  //     'questionText': '2. Which one of the following, if true, would most seriously undermine the scientist\'s hypothesis?',
-  //     'answers': [
-  //       {'text': 'Others', 'score': -2},
-  //       {'text': 'Principle', 'score': -2},
-  //       {'text': 'Implication', 'score': -2},
-  //       {'text': 'Weaken', 'score': 10
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     'contextText':
-  //         'Many breakfast cereals are fortified with vitamin supplements. Some of these cereals provide 100 percent of the recommended daily requirement of vitamins. Nevertheless, a well-balanced breakfast, including a variety of foods, is a better source of those vitamins than are such fortified breakfast cereals alone.',
-  //     'questionText': '3. Which of the following, if true, would most strongly support the position above?',
-  //     'answers': [
-  //       {'text': 'Sufficient Assumptions', 'score': -2},
-  //       {'text': 'Strengthen', 'score': 10},
-  //       {'text': 'Others', 'score': -2},
-  //       {'text': 'Identify a Flaw', 'score': -2},
-  //     ],
-  //   },
-  //   {
-  //     'contextText':
-  //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam ex, pharetra ac scelerisque vel, suscipit et sapien. Mauris eu enim eget nibh convallis aliquam non eu orci. In metus augue, vestibulum ut quam in, ultricies eleifend nulla. Vestibulum sit.',
-  //     'questionText': 'Q4. Who created Dart programing language?',
-  //     'answers': [
-  //       {'text': 'Lars Bak and Kasper Lund', 'score': 10},
-  //       {'text': 'Brendan Eich', 'score': -2},
-  //       {'text': 'Bjarne Stroustrup', 'score': -2},
-  //       {'text': 'Jeremy Ashkenas', 'score': -2},
-  //     ],
-  //   },
-  //   {
-  //     'contextText':
-  //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam ex, pharetra ac scelerisque vel, suscipit et sapien. Mauris eu enim eget nibh convallis aliquam non eu orci. In metus augue, vestibulum ut quam in, ultricies eleifend nulla. Vestibulum sit.',
-  //     'questionText':
-  //         'Q5. Is Flutter for Web and Desktop available in stable version?',
-  //     'answers': [
-  //       {
-  //         'text': 'Yes',
-  //         'score': -2,
-  //       },
-  //       {'text': 'No', 'score': 10},
-  //     ],
-  //   },
-  // ];
+  final _questions = const [
+    {
+      'contextText':
+          'Negotiations between the union and the trucking industry will not resume unless one or the other of these parties agrees to make concessions. The trucking industry will not make any concessions without certain prior concessions being made by the union, and the union will make concessions only if the government agrees to act as mediator.',
+      'questionText': '1. If the statements above are true, which one of the following must also be true on the basis of them?',
+      'answers': [
+        {'text': 'Identify a Flaw', 'score': -2},
+        {'text': 'Strengthen', 'score': -2},
+        {'text': 'Implication', 'score': 10},
+        {'text': 'Sufficient Assumptions', 'score': -2},
+      ],
+    },
+    {
+      'contextText':
+          'Scientists analyzing air bubbles that had been trapped in Antarctic ice during the Earth\'s last ice age found that the ice-age atmosphere had contained unusually large amounts of ferrous material and surprisingly small amounts of carbon dioxide. One scientist noted that algae absorb carbon dioxide from the atmosphere. The scientist hypothesized that the ferrous material, which was contained in atmospheric dust, had promoted a great increase in the population of Antarctic algae such as diatoms.',
+      'questionText': '2. Which one of the following, if true, would most seriously undermine the scientist\'s hypothesis?',
+      'answers': [
+        {'text': 'Others', 'score': -2},
+        {'text': 'Principle', 'score': -2},
+        {'text': 'Implication', 'score': -2},
+        {'text': 'Weaken', 'score': 10
+        },
+      ],
+    },
+    {
+      'contextText':
+          'Many breakfast cereals are fortified with vitamin supplements. Some of these cereals provide 100 percent of the recommended daily requirement of vitamins. Nevertheless, a well-balanced breakfast, including a variety of foods, is a better source of those vitamins than are such fortified breakfast cereals alone.',
+      'questionText': '3. Which of the following, if true, would most strongly support the position above?',
+      'answers': [
+        {'text': 'Sufficient Assumptions', 'score': -2},
+        {'text': 'Strengthen', 'score': 10},
+        {'text': 'Others', 'score': -2},
+        {'text': 'Identify a Flaw', 'score': -2},
+      ],
+    },
+    {
+      'contextText':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam ex, pharetra ac scelerisque vel, suscipit et sapien. Mauris eu enim eget nibh convallis aliquam non eu orci. In metus augue, vestibulum ut quam in, ultricies eleifend nulla. Vestibulum sit.',
+      'questionText': 'Q4. Who created Dart programing language?',
+      'answers': [
+        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
+        {'text': 'Brendan Eich', 'score': -2},
+        {'text': 'Bjarne Stroustrup', 'score': -2},
+        {'text': 'Jeremy Ashkenas', 'score': -2},
+      ],
+    },
+    {
+      'contextText':
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam ex, pharetra ac scelerisque vel, suscipit et sapien. Mauris eu enim eget nibh convallis aliquam non eu orci. In metus augue, vestibulum ut quam in, ultricies eleifend nulla. Vestibulum sit.',
+      'questionText':
+          'Q5. Is Flutter for Web and Desktop available in stable version?',
+      'answers': [
+        {
+          'text': 'Yes',
+          'score': -2,
+        },
+        {'text': 'No', 'score': 10},
+      ],
+    },
+  ];
 
   var _questionIndex = 0;
   var _totalScore = 0;
@@ -136,8 +139,11 @@ class _cateRecogState extends State<cateRecogDesign> {
           _questionIndex = _questionIndex - 1;
         } else if (index == 1) {
           print('Submit button');
+
+          print(temp);
+          print("check");
           // print(userExist());
-          //
+
           // db.collection('users').orderBy("first name").limit(6).get().then(
           //   (querySnapshot) {
           //     print("Successfully completed");
@@ -163,27 +169,14 @@ class _cateRecogState extends State<cateRecogDesign> {
       _questionIndex = 0;
       _totalScore = 0;
     });
-    // Future.delayed(const Duration(seconds: 2), () async {
-    //   setState(() {
-    //     _questionIndex = 0;
-    //     _totalScore = 0;
-    //   });
-    // });
   }
 
   void _answerQuestion(int score) {
     // + 1 no matter right or wrong for right now
     _totalScore += 1;
-
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    // Future.delayed(const Duration(seconds: 2), () async {
-    //   setState(() {
-    //     _questionIndex = _questionIndex + 1;
-    //   });
-    // });
-
     // ignore: avoid_print
     print(_questionIndex);
     if (_questionIndex < _questions.length) {
