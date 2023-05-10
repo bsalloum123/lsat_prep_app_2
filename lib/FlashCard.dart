fc2a49ff8918201110f71989636627e8745ab090
Kevine Shima <90464968+KJ-7@users.noreply.github.com>
2023-05-10 09:38:50 -0400

    Timer countdown code.

--- lib/FlashCard.dart
+++ lib/FlashCard.dart
@@ -1,11 +1,4 @@
-//Names: Batool, Christian, Inaya, Kevine
-//Description: FlashCard --> File creates flash cards for studying categories from
-//questions alone. Cards display question on one side and clicked to flip and
-//show category on the otherside. Arrow Navigation available with current card count.
-// Class: CSI CUE 2023
-
 import 'package:flip_card/flip_card.dart';
-//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lsat_prep_app_2/CardView.dart';
 import 'package:lsat_prep_app_2/FlashQuiz.dart';
@@ -14,15 +7,31 @@ import 'package:lsat_prep_app_2/main.dart';
 class FlashCard extends StatefulWidget {
   const FlashCard({Key? key}) : super(key: key);
 
+  // @override
+  // Widget build(BuildContext context){
+  //   return Scaffold(
+  //     debugShowCheckedModeBanner: false,
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.teal
+  //     ),
+  //     home: const FlashC(),
+  //   );
+  //
+  // }
+
   @override
   State<StatefulWidget> createState() {
     return _FlashCState();
   }
 }
+// class FlashC extends StatefulWidget{
+//   const FlashC({Key? key}) : super (key: key);
+//   @override
+//   _FlashCState createState() => _FlashCState();
+// }
 
 class _FlashCState extends State<FlashCard> {
-  // List of FlashQuiz objects to contain question,answer set pairs
-  // Uses same list as categoryRecognition, generated from Firestore database dataset
+
   final List<FlashQuiz> _FlashQuizes = [
     FlashQuiz(
         question: catRecList[1][0],
@@ -44,13 +53,11 @@ class _FlashCState extends State<FlashCard> {
         answer: catRecList[2][5]),
   ];
 
-  // Index of current card
   int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Title bar showing page title and help button
         appBar: AppBar(
           title: const Text('Flash Cards'),
           actions: <Widget>[
@@ -73,15 +80,11 @@ class _FlashCState extends State<FlashCard> {
                 height: 300,
                 child: Padding(
                   padding: const EdgeInsets.all(3.0),
-                  // Makes flippable card from the question and answer object
-                  // returned from CardView when given question and answer from
-                  // _FlashQuizes list.
                   child: FlipCard(
                     front: CardView(text: _FlashQuizes[_currentIndex].question),
                     back: CardView(text: _FlashQuizes[_currentIndex].answer),
                   ),
                 )),
-            // Arrows for navigation and middle for current card numbering num/max
             Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
               OutlinedButton.icon(
                   onPressed: showPreviousCard,
@@ -97,7 +100,6 @@ class _FlashCState extends State<FlashCard> {
         )));
   }
 
-  // Moves current index up, triggering a card, if more questions available
   void showNextCard() {
     setState(() {
       if (_currentIndex + 1 < _FlashQuizes.length) {
@@ -106,7 +108,6 @@ class _FlashCState extends State<FlashCard> {
     });
   }
 
-  // Moves current index down, triggering a card, if previous questions available
   void showPreviousCard() {
     setState(() {
       if (_currentIndex - 1 > -1) {
